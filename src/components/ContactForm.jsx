a9a3114ea0ce9eada7ff034a43a9a6e1c751ba25
Pricio1167 <noelpriciorakoto@gmail.com>
2026-09-17 13:23:08 +0300

    1

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,9 +3,9 @@ import emailjs from '@emailjs/browser'
 import { personal } from '../data/portfolio'
 import { CheckIcon, MailIcon, SendIcon, SpinnerIcon } from './icons'
 
-const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
-const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
-const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+const SERVICE_ID = 'service_yalwh2m'
+const TEMPLATE_ID = 'template_eotjrqf'
+const PUBLIC_KEY = 'vLGC_9kQRYYaWrDl-'
 
 const initialForm = { name: '', email: '', subject: '', message: '' }
 
